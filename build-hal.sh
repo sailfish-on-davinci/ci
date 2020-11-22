3d369dcac23b1381588fd4c358256fb600ab7f9c
BirdZhang <0312birdzhang@gmail.com>
2020-11-22 21:29:16 +0800

    add lost libs

--- build-hal.sh
+++ build-hal.sh
@@ -7,5 +7,12 @@ breakfast $DEVICE
 echo "clean .repo folder"
 rm -rf $ANDROID_ROOT/.repo
 
+# hack for droidmedia
+echo 'DROIDMEDIA_32 := true' >> external/droidmedia/env.mk
+echo 'FORCE_HAL:=1' >> external/droidmedia/env.mk
+
 make -j$(nproc --all) hybris-hal $(external/droidmedia/detect_build_targets.sh $PORT_ARCH $(gettargetarch)) \
  $(external/audioflingerglue/detect_build_targets.sh $PORT_ARCH $(gettargetarch))
+
+# Add vince lost libs
+cp device/xiaomi/vince/lostlibs/*.so out/target/product/vince/system/lib/
