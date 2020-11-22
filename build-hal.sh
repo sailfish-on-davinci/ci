4fa351b599f34696f37d73febb42a7dc76fb677c
BirdZhang <0312birdzhang@gmail.com>
2020-11-22 21:36:46 +0800

    update for droidmedia

--- build-hal.sh
+++ build-hal.sh
@@ -10,6 +10,8 @@ rm -rf $ANDROID_ROOT/.repo
 # hack for droidmedia
 echo 'DROIDMEDIA_32 := true' >> external/droidmedia/env.mk
 echo 'FORCE_HAL:=1' >> external/droidmedia/env.mk
+echo 'MINIMEDIA_AUDIOPOLICYSERVICE_ENABLE := 1' >> external/droidmedia/env.mk
+echo 'AUDIOPOLICYSERVICE_ENABLE := 1' >> external/droidmedia/env.mk
 
 make -j$(nproc --all) hybris-hal $(external/droidmedia/detect_build_targets.sh $PORT_ARCH $(gettargetarch)) \
  $(external/audioflingerglue/detect_build_targets.sh $PORT_ARCH $(gettargetarch))
