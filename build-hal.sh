42c3bb4a6d283a5740fed2c175cbe5208ab8e9f2
BirdZhang <0312birdzhang@gmail.com>
2020-12-02 10:14:43 +0800

    adapt for davinci

--- build-hal.sh
+++ build-hal.sh
@@ -13,8 +13,7 @@ echo 'FORCE_HAL:=1' >> external/droidmedia/env.mk
 echo 'MINIMEDIA_AUDIOPOLICYSERVICE_ENABLE := 1' >> external/droidmedia/env.mk
 echo 'AUDIOPOLICYSERVICE_ENABLE := 1' >> external/droidmedia/env.mk
 
-make -j$(nproc --all) hybris-hal $(external/droidmedia/detect_build_targets.sh $PORT_ARCH $(gettargetarch)) \
- $(external/audioflingerglue/detect_build_targets.sh $PORT_ARCH $(gettargetarch))
+# hybris-patches
+./hybris-patches/apply-patches.sh --mb
 
-# Add vince lost libs
-cp device/xiaomi/vince/lostlibs/*.so out/target/product/vince/system/lib/
+make -j$(nproc --all) hybris-hal $(external/droidmedia/detect_build_targets.sh $PORT_ARCH $(gettargetarch))
