6e6a821b1f5933e7deffa82c0a91dabcb77f38d8
BirdZhang <0312birdzhang@gmail.com>
2024-12-01 00:40:28 +0800

    update

--- build-hal.sh
+++ build-hal.sh
@@ -4,16 +4,26 @@ cd $ANDROID_ROOT
 source build/envsetup.sh 2>&1
 breakfast $DEVICE
 
+# jdk
+# /usr/lib/jvm/java-8-openjdk-amd64/
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+
+
 echo "clean .repo folder"
 rm -rf $ANDROID_ROOT/.repo
 
 # hack for droidmedia
-echo 'DROIDMEDIA_32 := true' >> external/droidmedia/env.mk
-echo 'FORCE_HAL:=1' >> external/droidmedia/env.mk
+cd external/droidmedia
+git checkout 0.20230605.1
+cd $ANDROID_ROOT
+# echo 'DROIDMEDIA_32 := true' >> external/droidmedia/env.mk
+# echo 'FORCE_HAL:=1' >> external/droidmedia/env.mk
 echo 'MINIMEDIA_AUDIOPOLICYSERVICE_ENABLE := 1' >> external/droidmedia/env.mk
 echo 'AUDIOPOLICYSERVICE_ENABLE := 1' >> external/droidmedia/env.mk
 
 # hybris-patches
 ./hybris-patches/apply-patches.sh --mb
 
-make -j$(nproc --all) hybris-hal $(external/droidmedia/detect_build_targets.sh $PORT_ARCH $(gettargetarch))
+make -j$(nproc --all) hybris-hal droidmedia
--- build-rpm.sh
+++ build-rpm.sh
@@ -2,9 +2,10 @@
 
 set -x
 
-source /home/nemo/work/ci/ci/hadk.env
+source /home/mersdk/work/ci/ci/hadk.env
+export ANDROID_ROOT=/home/mersdk/work/ci/ci/hadk_16.0
 
-sudo chown -R nemo:nemo $ANDROID_ROOT
+sudo chown -R mersdk:mersdk $ANDROID_ROOT
 cd $ANDROID_ROOT
 
 cd ~/.scratchbox2
@@ -20,17 +21,22 @@ sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R chmod 777 /boot
 sdk-assistant list
 
 cd $ANDROID_ROOT
-rpm/dhd/helpers/build_packages.sh --droid-hal
+sed -i '/CONFIG_NETFILTER_XT_MATCH_QTAGUID/d' hybris/mer-kernel-check/mer_verify_kernel_config
 
-DROIDMEDIA_VERSION=$(git --git-dir external/droidmedia/.git describe --tags | sed \
--r "s/\-/\+/g")
-rpm/dhd/helpers/pack_source_droidmedia-localbuild.sh $DROIDMEDIA_VERSION
-mkdir -p hybris/mw/droidmedia-localbuild/rpm
-cp rpm/dhd/helpers/droidmedia-localbuild.spec \
-hybris/mw/droidmedia-localbuild/rpm/droidmedia.spec
-sed -ie "s/0.0.0/$DROIDMEDIA_VERSION/" \
-hybris/mw/droidmedia-localbuild/rpm/droidmedia.spec
-mv hybris/mw/droidmedia-$DROIDMEDIA_VERSION.tgz hybris/mw/droidmedia-localbuild
-rpm/dhd/helpers/build_packages.sh --build=hybris/mw/droidmedia-localbuild
+sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R zypper in -y ccache
 
-rpm/dhd/helpers/build_packages.sh --droid-hal
+cd $ANDROID_ROOT
+# cp /home/mersdk/work/ci/ci/helpers/*.sh rpm/dhd/helpers/
+# chmod +x rpm/dhd/helpers/*.sh 
+
+cd $ANDROID_ROOT
+sudo mkdir -p /proc/sys/fs/binfmt_misc/
+sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+
+rpm/dhd/helpers/build_packages.sh
+
+if [ "$?" -ne 0 ];then
+  # if failed, retry once
+  rpm/dhd/helpers/build_packages.sh
+  cat $ANDROID_ROOT/droid-hal-$DEVICE.log
+fi
