57254b3cf96ec3f5f7ac6e5ab323170336973927
debo.zhang <0.0@birdzhang.xyz>
2025-12-18 12:33:47 +0800

    why cannot build now?

--- build-hal.sh
+++ build-hal.sh
@@ -2,9 +2,6 @@
 set -x
 
 source hadk.env
-cd $ANDROID_ROOT
-source build/envsetup.sh 2>&1
-breakfast $DEVICE
 
 # jdk
 # /usr/lib/jvm/java-8-openjdk-amd64/
@@ -12,23 +9,24 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export PATH=$JAVA_HOME/bin:$PATH
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 
-
-
-
 # hack for droidmedia
 cd $ANDROID_ROOT
 rm -rf external/droidmedia
 git clone --recurse-submodules https://github.com/sailfishos/droidmedia.git external/droidmedia
 cd external/droidmedia
 git checkout 0.20230605.1
-echo 'MINIMEDIA_AUDIOPOLICYSERVICE_ENABLE := 1' > external/droidmedia/env.mk
-echo 'AUDIOPOLICYSERVICE_ENABLE := 1' >> external/droidmedia/env.mk
+echo 'MINIMEDIA_AUDIOPOLICYSERVICE_ENABLE := 1' > env.mk
+echo 'AUDIOPOLICYSERVICE_ENABLE := 1' >> env.mk
 
 cd $ANDROID_ROOT/external
 git clone --recurse-submodules https://github.com/mer-hybris/libhybris.git
 # hybris-patches
 cd $ANDROID_ROOT
 hybris-patches/apply-patches.sh --mb
+
+cd $ANDROID_ROOT
+source build/envsetup.sh 2>&1
+breakfast $DEVICE
 
 # get more space
 rm -rf .repo
